2a69dacc0fa274c6fab234af22ba4cfd8f7b59c3
Inna_Cydeo <innazotikova@gmail.com>
2023-11-02 13:59:35 -0500

    updated information about Greti

--- src/utils/GretiSmells.java
+++ src/utils/GretiSmells.java
@@ -4,7 +4,7 @@ public class GretiSmells {
     public static void main(String[] args) {
 
         System.out.println("I'm not sure when it started but Greti smells, kind of horrible view.");
-
+        System.out.println("One more thing about her, she started wake up us every day, annoying.");
 
     }
 }
