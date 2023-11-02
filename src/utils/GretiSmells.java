46e7465cd561f48b3bd549f645983f5b4c33311e
Inna_Cydeo <innazotikova@gmail.com>
2023-11-02 13:57:49 -0500

    created scent of Greti

---
+++ src/utils/GretiSmells.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class GretiSmells {
+    public static void main(String[] args) {
+
+        System.out.println("I'm not sure when it started but Greti smells, kind of horrible view.");
+
+
+    }
+}
