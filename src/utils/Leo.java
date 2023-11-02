a682d353581e88fcc83d82af115581e381110f79
Inna_Cydeo <innazotikova@gmail.com>
2023-11-02 14:07:35 -0500

    Ballad about Leo is written

---
+++ src/utils/Leo.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Leo {
+    public static void main(String[] args) {
+        System.out.println("Ballad about Leo.");
+        System.out.println("hmmmrrr");
+        System.out.println("Eeeeech mooorninng this pice of shiiiit wakes me uuuup.");
+        System.out.println("It is more thaaaaan anoyiiiiing it iiisss unacceptaaaaaaableeeee.");
+    }
+}
